5cd494156336d62377fd108b0a56951afb54b218
Robin Cheng <robincheng999@gmail.com>
2018-09-25 23:06:28 -0400

    new changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
+    belongs_to :user
+
     def hello
         render html: 'hello, world!'
     end
